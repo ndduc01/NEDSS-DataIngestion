0cf51d9414b2b5f8444dc065b51986a58238ef6f
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2023-10-30 21:17:41 -0400

    Added condition to enable the routs.

--- report-service/src/main/java/gov/cdc/dataingestion/camel/routes/S3RouteBuilder.java
+++ report-service/src/main/java/gov/cdc/dataingestion/camel/routes/S3RouteBuilder.java
@@ -1,13 +1,16 @@
 package gov.cdc.dataingestion.camel.routes;
 
+import org.apache.camel.FailedToStartRouteException;
+import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "di.camel.s3-endpoint.enabled", havingValue = "true")
 public class S3RouteBuilder extends EndpointRouteBuilder {
     private static Logger logger = LoggerFactory.getLogger(S3RouteBuilder.class);
 
@@ -19,6 +22,13 @@ public class S3RouteBuilder extends EndpointRouteBuilder {
     @Override
     public void configure() throws Exception {
         logger.info("************ calling S3RouteBuilder *********");
+
+        onException(FailedToStartRouteException.class)
+                .log("FailedToStartRouteException s3RouteBuilder: ${exception}")
+                .handled(true);
+        onException(RuntimeCamelException.class).
+                handled(true)
+                .log("RuntimeCamelException s3RouteBuilder: ${exception}");
         System.out.println("************ calling S3RouteBuilder *********");
 //            from(aws2S3("di-manual-file-dropoff").delay(1000L)
 //                    .useDefaultCredentialsProvider(true).deleteAfterRead(true)).routeId("s3diRouteId")
@@ -28,13 +38,13 @@ public class S3RouteBuilder extends EndpointRouteBuilder {
 //                    .process(hL7FileProcessor)
 //                    .end();
 
-            from(aws2S3("di-manual-file-dropoff").delay(1000L)
-                    .useDefaultCredentialsProvider(true).deleteAfterRead(true)).routeId("s3diRouteId")
-                    .log("The file content from s3 is ${body}")
-                   // .setHeader("msgType", constant("HL7"))
-                   // .setHeader("validationActive", constant("false"))
-                    .to("bean:hL7FileProcessComponent")
-                    .end();
+        from(aws2S3("di-manual-file-dropoff").delay(1000L)
+                .useDefaultCredentialsProvider(true).deleteAfterRead(true)).routeId("s3diRouteId")
+                .log("The file content from s3 is ${body}")
+                // .setHeader("msgType", constant("HL7"))
+                // .setHeader("validationActive", constant("false"))
+                .to("bean:hL7FileProcessComponent")
+                .end();
     }
 //    @Bean
 //    public HL7FileProcessComponent hL7FileProcessComponent() {
--- report-service/src/main/java/gov/cdc/dataingestion/camel/routes/SFTPRouteBuilder.java
+++ report-service/src/main/java/gov/cdc/dataingestion/camel/routes/SFTPRouteBuilder.java
@@ -1,14 +1,18 @@
 package gov.cdc.dataingestion.camel.routes;
 
+import org.apache.camel.FailedToStartRouteException;
+import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "di.camel.sftp-endpoint.enabled", havingValue = "true")
 public class SFTPRouteBuilder extends RouteBuilder {
     private static Logger log = LoggerFactory.getLogger(SFTPRouteBuilder.class);
     @Value("${sftp.host}")
@@ -40,6 +44,12 @@ public class SFTPRouteBuilder extends RouteBuilder {
         System.out.println("sftp_server URL:" + ftp_server);
         //# for the server we want to delay 5 seconds between polling the server
         //# and keep the downloaded file as-is
+        onException(Exception.class)
+                .log("Exception sftpRouteBuilder: ${exception}")
+                .handled(true);
+        onException(RuntimeCamelException.class)
+                .log("RuntimeCamelException sftpRouteBuilder: ${exception}")
+                .handled(true);
 
             from(ftp_server).routeId("sftpRouteId")
                     .log("The file ${file:name} content from sftp server is ${body}")
