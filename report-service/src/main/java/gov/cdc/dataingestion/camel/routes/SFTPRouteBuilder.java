febbc1cee2266d4f69240af3cff0dc4339644846
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2023-11-02 13:00:20 -0400

    Update SFTPRouteBuilder.java
    Optimized the code.

--- report-service/src/main/java/gov/cdc/dataingestion/camel/routes/SFTPRouteBuilder.java
+++ report-service/src/main/java/gov/cdc/dataingestion/camel/routes/SFTPRouteBuilder.java
@@ -3,6 +3,7 @@ package gov.cdc.dataingestion.camel.routes;
 import org.apache.camel.FailedToStartRouteException;
 import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.net.URI;
 
 @Component
 @ConditionalOnProperty(name = "di.camel.sftp-endpoint.enabled", havingValue = "true")
@@ -34,29 +37,48 @@ public class SFTPRouteBuilder extends RouteBuilder {
         if (sftpDirectory == null || !sftpDirectory.startsWith("/")) {
             sftpDirectory = "/" + sftpDirectory;
         }
-        String ftp_server = "sftp://" + sftpHost + ":" + sftpPort + sftpDirectory + "?autoCreate=true&username=" + sftpUserName +
-                "&password=" + sftpPassword + "&passiveMode=true&binary=true&recursive=true" +
-                "&localWorkDirectory=files/download" +
-                "&delay=5000&noop=true&delete=true";
+
+        URI fromSftpUrl = new URIBuilder()
+                .setScheme("sftp")
+                .setHost(sftpHost)
+                .setPort(22)
+                .setPath(sftpDirectory)
+                .addParameter("username", sftpUserName)
+                .addParameter("password", sftpPassword)
+                .addParameter("autoCreate", "true")
+                .addParameter("passiveMode", "true")
+                .addParameter("initialDelay", "2000")
+                .addParameter("delay", "1000")
+                .addParameter("noop", "true")
+                .addParameter("delete", "true")
+                .addParameter("localWorkDirectory", "files/download") //check
+                .addParameter("recursive","true")
+                .addParameter("maximumReconnectAttempts","2")
+                .addParameter("reconnectDelay","5000")
+                .build();
         //ignoreFileNotFoundOrPermissionError
         //excludeExt=bak,da
-        log.debug("sftp_server URL:" + ftp_server);
-        System.out.println("sftp_server URL:" + ftp_server);
+        String sftp_server=fromSftpUrl.toString();
+        log.debug("sftp_server URL:" + sftp_server);
+        System.out.println("sftp_server URL:" + sftp_server);
         //# for the server we want to delay 5 seconds between polling the server
         //# and keep the downloaded file as-is
-        onException(Exception.class)
-                .log("Exception sftpRouteBuilder: ${exception}")
-                .handled(true);
-        onException(RuntimeCamelException.class)
-                .log("RuntimeCamelException sftpRouteBuilder: ${exception}")
-                .handled(true);
-
-            from(ftp_server).routeId("sftpRouteId")
+//        onException(Exception.class)
+//                .log("Exception sftpRouteBuilder: ${exception}")
+//                .handled(true);
+//        onException(RuntimeCamelException.class)
+//                .log("RuntimeCamelException sftpRouteBuilder: ${exception}")
+//                .handled(true);
+        System.out.println("fromSFtpUrl:"+sftp_server);
+            from(sftp_server).routeId("sftpRouteId")
                     .log("The file ${file:name} content from sftp server is ${body}")
                     ///validation
                     .to("bean:hL7FileProcessComponent")
                     .end();
 
+            //////
+
+        ////////////
     }
     @Bean
     public HL7FileProcessComponent hL7FileProcessComponent() {
