28606b4ca8f3c733ac175b3327b2f5c405580274
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2023-11-18 14:55:42 -0500

    Update S3RouteBuilderTest.java
    Updated unit testing

--- report-service/src/test/java/gov/cdc/dataingestion/camel/routes/S3RouteBuilderTest.java
+++ report-service/src/test/java/gov/cdc/dataingestion/camel/routes/S3RouteBuilderTest.java
@@ -8,6 +8,8 @@ import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.io.File;
+
 import static org.apache.camel.builder.AdviceWith.adviceWith;
 
 class S3RouteBuilderTest extends CamelTestSupport {
@@ -26,22 +28,56 @@ class S3RouteBuilderTest extends CamelTestSupport {
 
     @Test
     public void testMockEndpoints() throws Exception {
-        RouteDefinition route = context.getRouteDefinition("s3diRouteId");
+        RouteDefinition routeS3Main = context.getRouteDefinition("s3diRouteId");
+        RouteDefinition routeS3UnzipFile = context.getRouteDefinition("s3UnzipFileRouteId");
+        RouteDefinition routeS3ReadFromUnzipDir = context.getRouteDefinition("s3ReadFromUnzipDirRouteId");
+
         adviceWith(
-                route,
+                routeS3Main,
                 context,
                 new AdviceWithRouteBuilder() {
                     @Override
                     public void configure() throws Exception {
-                        replaceFromWith("direct:fromS3Route");
-                        weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:results3");
+                        replaceFromWith("direct:s3DIMainRoute");
+                        weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:s3DIMainResult");
+                    }
+                });
+        adviceWith(
+                routeS3UnzipFile,
+                context,
+                new AdviceWithRouteBuilder() {
+                    @Override
+                    public void configure() throws Exception {
+                        replaceFromWith("direct:s3UnzipFileRoute");
+                        weaveByToUri("file:files/s3UnzipDownload").replace().to("mock:s3UnzipFileResult");
+                    }
+                });
+        adviceWith(
+                routeS3ReadFromUnzipDir,
+                context,
+                new AdviceWithRouteBuilder() {
+                    @Override
+                    public void configure() throws Exception {
+                        replaceFromWith("direct:s3ReadFromUnzipDirRoute");
+                        weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:s3ReadFromUnzippedFilesResult");
                     }
                 });
         context.start();
 
-        MockEndpoint mock = getMockEndpoint("mock:results3");
+        //For s3diRouteId
+        MockEndpoint mock = getMockEndpoint("mock:s3DIMainResult");
         mock.expectedMessageCount(1);
-        template.sendBody("direct:fromS3Route", "HL7 Test message from S3 Route");
+        template.sendBody("direct:s3DIMainRoute", "HL7 Test message from S3 main Route");
         mock.assertIsSatisfied();
+        //For s3UnzipFileRouteId
+        MockEndpoint mockSftpUnzipFile = getMockEndpoint("mock:s3UnzipFileResult");
+        mockSftpUnzipFile.expectedMinimumMessageCount(4);
+        template.sendBody("direct:s3UnzipFileRoute", new File("src/test/resources/hl7testdata.zip"));
+        mockSftpUnzipFile.assertIsSatisfied();
+        ////For s3ReadFromUnzipDirRouteId
+        MockEndpoint mockSftpReadFromUnzippedFiles = getMockEndpoint("mock:s3ReadFromUnzippedFilesResult");
+        mockSftpReadFromUnzippedFiles.expectedMessageCount(1);
+        template.sendBody("direct:s3ReadFromUnzipDirRoute", "HL7 Test message from S3 Route");
+        mockSftpReadFromUnzippedFiles.assertIsSatisfied();
     }
 }
