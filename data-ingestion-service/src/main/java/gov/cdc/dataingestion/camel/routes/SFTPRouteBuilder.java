d73484f720a01fd18b4e914c5f86c28fbe9c307d
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-01-13 13:10:01 -0500

    Implemented to move the files to processed and unprocessed files.

--- data-ingestion-service/src/main/java/gov/cdc/dataingestion/camel/routes/SFTPRouteBuilder.java
+++ data-ingestion-service/src/main/java/gov/cdc/dataingestion/camel/routes/SFTPRouteBuilder.java
@@ -1,5 +1,6 @@
 package gov.cdc.dataingestion.camel.routes;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.zipfile.ZipSplitter;
 import org.apache.http.client.utils.URIBuilder;
@@ -39,7 +40,7 @@ public class SFTPRouteBuilder extends RouteBuilder {
             sftpDirectory = "/" + sftpDirectory;
         }
 
-        URI fromSftpUrl = new URIBuilder()
+        URI sftpUriBuilder = new URIBuilder()
                 .setScheme("sftp")
                 .setHost(sftpHost)
                 .setPort(22)
@@ -51,48 +52,103 @@ public class SFTPRouteBuilder extends RouteBuilder {
                 .addParameter("initialDelay", "2000")
                 .addParameter("delay", "1000")
                 .addParameter("noop", "true")
-                .addParameter("delete", "true")
+                .addParameter("delete", "true")//check
                 .addParameter("localWorkDirectory", "files/download") //check
-                .addParameter("recursive","true")
+                .addParameter("recursive","false")//check
                 .addParameter("maximumReconnectAttempts","2")
                 .addParameter("reconnectDelay","5000")
-                .addParameter("excludeExt","bak,da")
-                .addParameter("includeExt","txt,TXT,zip,ZIP")
+                .addParameter("useUserKnownHostsFile","false")
+//                .addParameter("excludeExt","bak,da")
+//                .addParameter("includeExt","txt,TXT,zip,ZIP")
                 .build();
-
-        String sftp_server=fromSftpUrl.toString();
-        log.debug("sftp_server URL:" + sftp_server);
+        URI sftpUriProcessed = new URIBuilder()
+                .setScheme("sftp")
+                .setHost(sftpHost)
+                .setPort(22)
+                .setPath(sftpDirectory+"processed")
+                .addParameter("username", sftpUserName)
+                .addParameter("password", sftpPassword)
+                .addParameter("autoCreate", "true")
+                .addParameter("useUserKnownHostsFile","false")
+                //.addParameter("includeExt","txt,TXT,zip,ZIP")
+                .build();
+        URI sftpUriUnProcessed = new URIBuilder()
+                .setScheme("sftp")
+                .setHost(sftpHost)
+                .setPort(22)
+                .setPath(sftpDirectory+"unprocessed")
+                .addParameter("username", sftpUserName)
+                .addParameter("password", sftpPassword)
+                .addParameter("autoCreate", "true")
+                .addParameter("useUserKnownHostsFile","false")
+                //.addParameter("excludeExt","txt,TXT,zip,ZIP")
+                .build();
+//move
+//moveFailed
+//preMove
+        String sftpServer=sftpUriBuilder.toString();
+        log.debug("sftp_server URL:" + sftpServer);
         //# for the server we want to delay 5 seconds between polling the server
         //# and keep the downloaded file as-is
 
         log.debug("Calling sftpRouteId");
         //Download the file from sftp server.If the file is zip, it will be downloaded into files/sftpdownload directory.
         //If it's a text file, it will be processed.
-        from(sftp_server).routeId("sftpRouteId")
+        from(sftpServer).routeId("sftpRouteId")
                 .log("The file from sftpRouteId: ${file:name}")
                 .choice()
                     .when(simple("${file:name} endsWith '.zip'"))
-                        .log(" when .zip condition...The file ${file:name}")
+                        .log(" *****when .zip condition...The file ${file:name}")
                         .to("file:files/sftpdownload")
                     .otherwise()
-                        .log(" Otherwise condition for txt files ...The file ${file:name} content from sftp server is ${body}")
-                        .to("bean:hL7FileProcessComponent")
-                .end();
+                        .log(" ****Otherwise condition for other files ...The file ${file:name} content from sftp server is ${body}")
+                        .to("file:files/sftpUnzipDownload")
+        .end();
         // Unzip the downloaded file
         log.debug("Calling sftpUnzipFileRouteId");
         from("file:files/sftpdownload")
                 .routeId("sftpUnzipFileRouteId")
                 .split(new ZipSplitter()).streaming()
                 .to("file:files/sftpUnzipDownload")
-                .end();
+        .end();
 
         //Process the files from unzipped folder
         log.debug("Calling sftpdownloadUnzip");
-        from("file:files/sftpUnzipDownload?includeExt=txt")
+        from("file:files/sftpUnzipDownload")//file:files/sftpUnzipDownload?includeExt=txt
                 .routeId("SftpReadFromUnzipDirRouteId")
                 .log(" Read from unzipped files folder ...The file ${file:name}")
-                .to("bean:hL7FileProcessComponent")
+                .to("seda:processfiles","seda:movefiles")
                 .end();
+        from("seda:processfiles")
+                .routeId("sedaProcessFilesRouteId")
+                .log("from seda processfiles file: ${file:name}")
+                .choice()
+                    .when(simple("${file:name} endsWith '.txt'"))
+                        .log("File processed:${file:name}")
+                        .to("bean:hL7FileProcessComponent")
+                    .otherwise()
+                        .log("File not processed:${file:name}")
+                .endChoice()
+        .end();
+
+        from("seda:movefiles")
+                .routeId("sedaMoveFilesRouteId")
+                .log("from seda movefiles file:${file:name} body: ${body}")
+                .to("file:files/sftpProcessedUnprocessed")
+        .end();
+
+        from("file:files/sftpProcessedUnprocessed")
+                .log("from files sftpProcessedUnprocessed The file ${file:name}")
+                .delay(5000)
+                .setHeader(Exchange.FILE_NAME,simple("${date:now:yyyyMMddHHmmssSSS}-${file:name}"))
+                .choice()
+                    .when(simple("${file:name} endsWith '.txt'"))
+                        .to(sftpUriProcessed.toString())
+                    .otherwise()
+                        .to(sftpUriUnProcessed.toString())
+                .endChoice()
+        .end();
+
+        //pollEnrich
     }
  }
-
--- data-ingestion-service/src/test/java/gov/cdc/dataingestion/camel/routes/SFTPRouteBuilderTest.java
+++ data-ingestion-service/src/test/java/gov/cdc/dataingestion/camel/routes/SFTPRouteBuilderTest.java
@@ -1,33 +1,16 @@
 package gov.cdc.dataingestion.camel.routes;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.EndpointInject;
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.model.RouteDefinition;
 import org.apache.camel.test.junit5.CamelTestSupport;
-import org.apache.camel.test.spring.junit5.*;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.io.File;
 
 import static org.apache.camel.builder.AdviceWith.adviceWith;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 
 class SFTPRouteBuilderTest extends CamelTestSupport {
@@ -47,18 +30,21 @@ class SFTPRouteBuilderTest extends CamelTestSupport {
 
     @Test
     public void testMockEndpoints() throws Exception {
-        RouteDefinition route = context.getRouteDefinition("sftpRouteId");
+        RouteDefinition sftpRoute = context.getRouteDefinition("sftpRouteId");
         RouteDefinition routeSftpUnzipFile = context.getRouteDefinition("sftpUnzipFileRouteId");
         RouteDefinition routeSftpReadFromUnzipDir = context.getRouteDefinition("SftpReadFromUnzipDirRouteId");
+        RouteDefinition routeSedaProcessFiles = context.getRouteDefinition("sedaProcessFilesRouteId");
 
         adviceWith(
-                route,
+                sftpRoute,
                 context,
                 new AdviceWithRouteBuilder() {
                     @Override
                     public void configure() throws Exception {
                         replaceFromWith("direct:fromSftpRoute");
-                        weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:result");
+                        //weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:result");
+                        weaveByToUri("file:files/sftpUnzipDownload").replace().to("mock:result");
+
                     }
                 });
         adviceWith(
@@ -78,7 +64,21 @@ class SFTPRouteBuilderTest extends CamelTestSupport {
                     @Override
                     public void configure() throws Exception {
                         replaceFromWith("direct:sftpReadFromUnzipDirRoute");
-                        weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:sftpReadFromUnzippedFilesResult");
+                       // weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:sftpReadFromUnzippedFilesResult");
+                        weaveByToUri("seda:processfiles").replace().to("mock:sftpReadFromUnzippedFilesResult");
+                    }
+                });
+        adviceWith(
+                routeSedaProcessFiles,
+                context,
+                new AdviceWithRouteBuilder() {
+                    @Override
+                    public void configure() throws Exception {
+                        replaceFromWith("seda:sedaProcessFilesRoute");
+                        // weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:sftpReadFromUnzippedFilesResult");
+                        weaveByToUri("bean:hL7FileProcessComponent*").replace().to("mock:processBodyResult1");
+                        weaveAddLast().to("mock:processBodyResult");
+
                     }
                 });
         context.start();
@@ -86,17 +86,29 @@ class SFTPRouteBuilderTest extends CamelTestSupport {
         //For sftpRouteId
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMessageCount(1);
-        template.sendBody("direct:fromSftpRoute", "HL7 Test message from SFTP Route");
+        //template.sendBody("direct:fromSftpRoute", "HL7 Test message from SFTP Route");
+        template.sendBody("direct:fromSftpRoute", new File("src/test/resources/sftpfiles/HL7file-test.txt"));
         mock.assertIsSatisfied();
+
         //For sftpUnzipFileRouteId
         MockEndpoint mockSftpUnzipFile = getMockEndpoint("mock:sftpUnzipFileResult");
         mockSftpUnzipFile.expectedMinimumMessageCount(4);
-        template.sendBody("direct:sftpUnzipFileRoute", new File("src/test/resources/hl7testdata.zip"));
+        template.sendBody("direct:sftpUnzipFileRoute", new File("src/test/resources/sftpfiles/hl7testdata.zip"));
         mockSftpUnzipFile.assertIsSatisfied();
         ////
         MockEndpoint mockSftpReadFromUnzippedFiles = getMockEndpoint("mock:sftpReadFromUnzippedFilesResult");
         mockSftpReadFromUnzippedFiles.expectedMessageCount(1);
-        template.sendBody("direct:sftpReadFromUnzipDirRoute", "HL7 Test message from SFTP Route");
+       // template.sendBody("direct:sftpReadFromUnzipDirRoute", "HL7 Test message from SFTP Route");
+        template.sendBody("direct:sftpReadFromUnzipDirRoute", new File("src/test/resources/sftpfiles/HL7file-test.txt"));
+      //  template.sendBody("direct:sftpReadFromUnzipDirRoute", "HL7 Test message from SFTP Route");
         mockSftpReadFromUnzippedFiles.assertIsSatisfied();
+
+        MockEndpoint mockProcessBody = getMockEndpoint("mock:processBodyResult");
+        mockProcessBody.expectedMessageCount(2);
+        // template.sendBody("direct:sftpReadFromUnzipDirRoute", "HL7 Test message from SFTP Route");
+        template.sendBody("seda:sedaProcessFilesRoute", new File("src/test/resources/sftpfiles/HL7file-test1.txt"));
+        template.sendBody("seda:sedaProcessFilesRoute", new File("src/test/resources/sftpfiles/HL7file-test.txt"));
+       // template.sendBody("seda:sedaProcessFilesRoute", "HL7 Test message from SFTP Route");
+        mockProcessBody.assertIsSatisfied();
     }
 }
